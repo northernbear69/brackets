18cce0d73d6abc62fdfd1f484a930494359f2daa
northernbear69 <northernbear69webdev@gmail.com>
2021-02-27 21:47:43 +0300

    feat: add func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(str.length % 2 !== 0) return false;
+  
+  bracketsConfig = bracketsConfig.map(el => el.join(''));
+  let isDelete = true;
+  let checkStr;
+
+  while(isDelete) {
+    checkStr = str;
+    bracketsConfig.forEach(element => {
+      str = str.replace(element, '');
+    });
+    if(checkStr === str) isDelete = false;
+  }
+  
+  return str.length === 0;
 }
